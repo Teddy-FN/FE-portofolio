52f9e00c717c2be00366263865ddcb7343c0c71b
Teddy-FN <tedd.ferdy@gmail.com>
2024-10-31 15:44:51 +0700

    Adding Photo

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Full Stack Developer</span>
             <h1 className="h1">
               Hello Im <br />
@@ -41,7 +41,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
