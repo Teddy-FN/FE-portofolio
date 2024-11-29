0bbde70c1bf0310b02b0ff2f8bc73110b747d79f
Teddy-FN <tedd.ferdy@gmail.com>
2024-11-29 10:04:32 +0700

    CRUD Skills

---
+++ service/skills.js
@@ -0,0 +1,60 @@
+import { axiosInstance } from ".";
+
+export const getListSkills = async () => {
+  const { data, status } = await axiosInstance.get(
+    `/skills/get-skills?isTable=false`
+  );
+
+  if (status !== 200) throw Error(`${data.message}`);
+  return data;
+};
+
+// Get List skills In Table
+export const getListTableSkills = async ({ page, limit }) => {
+  const { data, status } = await axiosInstance.get(
+    `/skills/get-skills?isTable=true&page=${page}&limit=${limit}`
+  );
+
+  if (status !== 200) throw Error(`${data.message}`);
+  return data;
+};
+
+// Get By Id
+export const getSkillsById = async (payload) => {
+  const { data, status } = await axiosInstance.get(
+    `/skills/get-skills/${payload.id}`
+  );
+
+  if (status !== 200) throw Error(`${data.message}`);
+  return data;
+};
+
+// Post skills
+export const postSkills = async (payload) => {
+  const { data, status } = await axiosInstance.post(
+    "/skills/add-skills",
+    payload
+  );
+
+  if (status !== 200) throw Error(`${data.message}`);
+  return data;
+};
+
+export const putSkills = async ({ id, body }) => {
+  const { data, status } = await axiosInstance.put(
+    `/skills/edit-skills/${id}`,
+    body
+  );
+
+  if (status !== 200) throw Error(`${data.message}`);
+  return data;
+};
+
+export const deleteSkills = async ({ id }) => {
+  const { data, status } = await axiosInstance.delete(
+    `/skills/delete-skills/${id}`
+  );
+
+  if (status !== 200) throw Error(`${data.message}`);
+  return data;
+};
